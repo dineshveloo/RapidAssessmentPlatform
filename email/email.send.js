fa1f30da889c5b186f9d49f246668ff17cab65ed
Lohitha-garigipati <63113044+Lohitha-garigipati@users.noreply.github.com>
2020-08-07 12:30:59 +0530

    Update email.send.js

--- email/email.send.js
+++ email/email.send.js
@@ -26,7 +26,7 @@ const email = function (newUser) {
 
   let info = transporter.sendMail({
     from: process.env.MAIL_USER,
-    to: "anurao96@gmail.com", // list of receivers
+    to: "anurao96@gmail.com", "veena.S01@mphasis.com", // list of receivers
     subject: "Access Request to RAP", // Subject line
     html: htmlTemplate
   })
